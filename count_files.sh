537b01f76d751f274ff36d2116636e33bb73aa0f
Epiphane-code <omarepiphanefarouk@gmail.com>
2025-05-12 08:17:28 +0100

    retirer un espace

--- count_files.sh
+++ count_files.sh
@@ -6,4 +6,4 @@ dossier=$(basename "$(pwd)")
 nombre=$( ls -l | grep ^- | wc -l )
 echo -e "$dossier"
 echo ""
-echo "Le dossier $dossier contient $nombre fichiers(s) ."
+echo "Le dossier $dossier contient $nombre fichiers(s)."
