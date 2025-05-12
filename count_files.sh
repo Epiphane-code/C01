f1d4417d55a023c3e91626e16f13e1cf34df542d
Epiphane-code <omarepiphanefarouk@gmail.com>
2025-05-12 08:14:24 +0100

    ajout commentaire

--- count_files.sh
+++ count_files.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-dossier=$(basename "$(pwd)")
+# Ce code permet de compter de d'afficher le nombre des fichiers
+# variable dossier recupère le nom de base du repertoire courant
+dossier=$(basename "$(pwd)") 
+# variable 'nombre' recupère le nombre des fichiers reguliers
 nombre=$( ls -l | grep ^- | wc -l )
 echo -e "$dossier"
 echo ""
