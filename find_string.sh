5abaa18b250d7956bff15f1c9cc7c606b46aadd6
Epiphane-code <omarepiphanefarouk@gmail.com>
2025-05-12 08:57:44 +0100

    suppression

--- find_string.sh
+++ find_string.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Ce code permet de verifier si un mot se trouve dans un fichier
-echo "Entrez le nom du fichier"
+echo ""
 read file
-echo "Entrez le mot a rechercher"
+echo ""
 read mot
-echo -e "\n$file\n"
+echo ""
 if  grep -q "$mot" "$file"; then
 echo "La chaîne '$mot' a été trouvée dans $file"
 else
