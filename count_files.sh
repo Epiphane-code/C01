fe4f23a729cbae12f9cc6ec025b966fbf2c34b9e
Epiphane-code <omarepiphanefarouk@gmail.com>
2025-05-12 08:06:15 +0100

    modi

--- count_files.sh
+++ count_files.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 dossier=$(basename "$(pwd)")
 nombre=$( ls -l | grep ^- | wc -l )
-echo -e "$dossier\n"
-echo "Le dossier $dossier contient $nombre fichiers(s)."
+echo -e "$dossier"
+echo ""
+echo "Le dossier $dossier contient $nombre fichiers(s) ."
