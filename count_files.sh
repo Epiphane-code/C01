553909c45a2816d024898b0d9b0dc372b4eca0d5
Epiphane-code <omarepiphanefarouk@gmail.com>
2025-05-12 02:05:57 +0100

    Update count_files.sh
    ajout d'un retour à la ligne

--- count_files.sh
+++ count_files.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 basename "$(pwd)"
 dossier=$(basename "$(pwd)")
-echo "Le dossier $dossier contient $(ls -l | grep ^- | wc -l) fichiers(s)."
+echo -e "\nLe dossier $dossier contient $(ls -l | grep ^- | wc -l) fichiers(s)."
