bc636a1ff89f3a6f08a6b6e60dbea03140bc69f2
Epiphane-code <omarepiphanefarouk@gmail.com>
2025-05-13 18:14:57 +0100

    Delete count_files.sh

--- count_files.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Entrez le nom du dossier"
-read dossier
-while [ ! -d $dossier ] 
-do
-    read dos
-    dossier=$dos 
-done
-echo "Le dossier $dossier contient $(ls -p "$dossier" | wc -l) fichier(s)."
