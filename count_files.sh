9eac89d16df476c0abaf35bbecaa5bd01fe0aac3
Epiphane-code <omarepiphanefarouk@gmail.com>
2025-05-15 01:09:00 +0200

    Update count_files.sh

--- count_files.sh
+++ count_files.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #  sorkho je te salue 😂
+# un script qui permet de compter le nombre des fichiers dans un dossier
 read myfolder
-nb=$(ls -1 $myfolder | wc -l)
-echo "Le dossier $myfolder contient $nb fichier(s)."
+nb=$(ls -1 "$myfolder" | wc -l)
+echo "Le dossier "$myfolder" contient "$nb" fichier (s)."
