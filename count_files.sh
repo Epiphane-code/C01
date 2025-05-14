77ff68733e242ca422a9ca8486a7d1f38057ac1f
Epiphane-code <omarepiphanefarouk@gmail.com>
2025-05-14 17:36:19 +0100

    Update count_files.sh

--- count_files.sh
+++ count_files.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 read myfolder
 nb=$(ls "$myfolder" | wc -l)
-if [ $nb -gt 0 ]; then
 echo "Le dossier "$myfolder" contient "$nb" fichier(s)."
-else
-echo "Le dossier "$myfolder" contient 0 fichier(s)."
-fi
-exit 0
