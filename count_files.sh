df890179d0624fafec9cc896d5a9b2ca655b9d4d
Epiphane-code <omarepiphanefarouk@gmail.com>
2025-05-14 17:20:12 +0100

    Update count_files.sh

--- count_files.sh
+++ count_files.sh
@@ -2,7 +2,7 @@
 read dossier
 nb=$(ls "$dossier" | wc -l)
 if [ $nb -gt 0 ]; then
-echo "Le dossier $dossier contient "$nb" fichier(s)."
+echo "Le dossier "$dossier" contient "$nb" fichier(s)."
 else
-echo "Le dossier $dossier contient 0 fichier(s)."
+echo "Le dossier "$dossier" contient 0 fichier(s)."
 fi
