10c90d93148b80fef89af72f83c78cf693ad6fef
Epiphane-code <omarepiphanefarouk@gmail.com>
2025-05-14 14:14:02 +0100

    Update count_files.sh

--- count_files.sh
+++ count_files.sh
@@ -4,5 +4,5 @@ while [ ! -d $dossier ]; do
 read dos
 dossier=$dos
 done
-res=$(ls "$dossier"| wc -l)
+res=$(ls -p "$dossier"| grep -v / | wc -l)
 echo "Le dossier $dossier contient $res fichier(s)."
