c7af8711d5f6feb7e76de5437b249a4742e336cd
Epiphane-code <omarepiphanefarouk@gmail.com>
2025-05-15 23:00:08 +0200

    Update count_files.sh

--- count_files.sh
+++ count_files.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # un script qui permet de compter le nombre des fichiers dans un dossier
-#lire le nom du dossier
 read "dos"
-nb=$((find $dos -maxdepth 1 -type f | wc -l))
+nb=$(ls -1 | wc -l)
 echo "Le dossier $dos contient $nb fichier(s)."
