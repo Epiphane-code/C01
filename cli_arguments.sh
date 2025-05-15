ef4ac8d1d2d75acbebc696823b4f365a66d3d393
Epiphane-code <omarepiphanefarouk@gmail.com>
2025-05-15 10:18:03 +0200

    Update cli_arguments.sh

--- cli_arguments.sh
+++ cli_arguments.sh
@@ -1,7 +1,6 @@
 #!/bin/bash 
-read -a arg                        #lire les arguments dans un tableau
-k=1                                #initialisation
-for i in ${arg[@]}; do             #parcourire les arguments 
-echo "Argument $k: ${arg[$i]} "             #afficher les arguments
-$(($k++))                        #incrementer k
-done                               #fin de la boucle
+arg=("$@") 
+echo "$@"
+for i in ${!arg[@]}; do 
+echo "Argument $i: ${arg[$i]}"
+done                 
