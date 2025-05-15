8ab33d5cd9913d846fa0af2355017fd629944fe2
Epiphane-code <omarepiphanefarouk@gmail.com>
2025-05-15 01:23:44 +0100

    Update cli_arguments.sh

--- cli_arguments.sh
+++ cli_arguments.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+#lire les arguments 
 read -a arg
+
+#inialiser le compteur 
 k=1
-for i in ${arg[@]} do
-Argument $k: $(arg[$k])
-k=$(($k+1))
+
+#parcourir les arguments et les afficher
+for i in "${arg[@]}"; do
+  echo "Argument $k: $i"
+  ((k++))
 done
