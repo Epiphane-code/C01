fa1e329c2a31475c2b9cefc61da1352603a4e814
Epiphane-code <omarepiphanefarouk@gmail.com>
2025-05-15 14:37:47 +0200

    Update count_files.sh

--- count_files.sh
+++ count_files.sh
@@ -2,4 +2,4 @@
 # un script qui permet de compter le nombre des fichiers dans un dossier
 read d
 # affichage du resultat
-echo "Le dossier $d contient $(ls -1 $d | wc -l) fichier(s)."
+echo "Le dossier $d contient $(ls -p $d | grep -v / | wc -l) fichier(s)."
