acaffca103146811cc6375b4f31676c484173456
Epiphane-code <omarepiphanefarouk@gmail.com>
2025-05-15 09:48:09 +0200

    Update cli_arguments.sh

--- cli_arguments.sh
+++ cli_arguments.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-echo "$@"      #afficher tous les arguments separes sous une chaine
+read -a arg    #afficher tous les arguments separes sous une chaine
 k=1            #initialisation
-for i in $@; do
+for i in ${arg[@]}; do
 echo "Argument $k: $i"
 k=$(($k+1))
 done
