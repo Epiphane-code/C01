53ad664a45c6715d98bea9eac8a5f6cab03220ec
Epiphane-code <omarepiphanefarouk@gmail.com>
2025-05-15 14:10:36 +0200

    Update simple_calculator.sh

--- simple_calculator.sh
+++ simple_calculator.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+#Entrez les valeurs et un operateur
 read a b op
+#Instruction en fonction de l'operateur
 if [ "$op" == "+" ]; then
 echo "Résultat : $(($a+$b))"
 exit 0
@@ -12,3 +14,4 @@ exit 0
 else
 echo "Résultat : $(($a/$b))"
 exit 0
+fi
