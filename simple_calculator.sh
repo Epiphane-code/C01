1c420aa11c97f87f870a8f74fb9ff7c6c4ac300b
Epiphane-code <omarepiphanefarouk@gmail.com>
2025-05-16 18:37:24 +0200

    Update simple_calculator.sh

--- simple_calculator.sh
+++ simple_calculator.sh
@@ -2,18 +2,18 @@
 read a b op
 case "$op" in
 "+")
-echo "Résultat : "$((a + b))"";;
+echo "Résultat : "$(($a+$b))"";;
 "-" )
-echo "Résultat : "$((a - b))"";;
+echo "Résultat : "$(($a-$b))"";;
 "/" )
 if [ $b -eq 0 ]; then
 echo "Impossible"
 else
-echo "Résultat : "$((a / b))""
+echo "Résultat : "$(($a/$b))""
 fi ;;
-"*")
-echo "Résultat : "$((a * b))"";;
-*)
+\* )
+echo "Résultat : "$(($a*$b))"";;
+* )
 echo "Operateur invalide";; 
 esac
 exit 0
