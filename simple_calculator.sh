a205d432bbf6235563afa3416828e827d8753c31
Epiphane-code <omarepiphanefarouk@gmail.com>
2025-05-16 13:32:22 +0200

    Update simple_calculator.sh

--- simple_calculator.sh
+++ simple_calculator.sh
@@ -1,5 +1,10 @@
-read a b op
-res=$(echo "$a $op $b" | bc -l 2>/dev/null)
-if [ ! -z $res ]; then
-echo "Résultat : $res"
-fi
+#!/bin/bash
+  read folder
+  nb=$(
+  for item in $folder/*; do
+  if [ -f $item ]; then
+  ls $item
+  fi
+  done | wc -l
+  )
+  echo "Le dossier $folder contient $nb fichier(s)."
