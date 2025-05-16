2478bc3ed95953212dd378a639feb56322bfe1a2
Epiphane-code <omarepiphanefarouk@gmail.com>
2025-05-16 18:12:58 +0200

    Update simple_calculator.sh

--- simple_calculator.sh
+++ simple_calculator.sh
@@ -2,17 +2,17 @@
 read a b op
 case "$op" in
 "+")
-res=$(($a+$b));;
+res=$($a+$b);;
 "-" )
-res=$(($a-$b));;
+res=$($a-$b);;
 "/" )
 if [ $b -eq 0 ]; then
 res="Impossible"
 else
-res=$(($a/$b))
+res=$($a/$b)
 fi ;;
 \* )
-res=$(($a*$b));;
+res=$($a*$b);;
 * )
 res="operation invalide";; 
 esac
