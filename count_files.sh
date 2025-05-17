bd80d8a4cac3bb6c553f7439517a3b8465706ed1
Epiphane-code <omarepiphanefarouk@gmail.com>
2025-05-17 20:56:05 +0100

    Update count_files.sh

--- count_files.sh
+++ count_files.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-#sorkho
-  read Myfolder
-  count=$(ls "$Myfolder" | wc -l)
-  if [ "$count" ]; then
+# Demande à l'utilisateur de saisir le nom du dossier
+read Myfolder
+#comptage des fichiers
+count=$(ls "$Myfolder" | wc -l)
+# Affiche le résultat
+if [ "$count" ]; then
+  #Affichage du nombre de fichies dans le dossier saisi
   echo "Le dossier $Myfolder contient $count fichier(s)."
-  fi
+fi
